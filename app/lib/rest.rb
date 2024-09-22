647b53d37adb479d005b61cdcc47fab1dbfcecb7
David Coomber <47242934+dcoomber@users.noreply.github.com>
2024-09-22 13:08:51 +0200

    General update

--- app/lib/rest.rb
+++ app/lib/rest.rb
@@ -12,7 +12,6 @@ def history_request(url, game_name)
   end_date = Date.today.strftime('%d/%m/%Y')
 
   request = Net::HTTP::Post.new(uri)
-  request["Cookie"] = "2b42a5900eca15e8f83f64787af73f39=d9k8omsqb9fgm6lij9tf6u76i3; NSC_ESNS=1e5de8c5-3da6-143a-9678-e638e06afb3d_0597323114_2818328314_00000000026279107854"
   form_data = [['gameName', game_name],['startDate', start_date],['endDate', end_date],['offset', '0'],['limit', '51']]
   request.set_form form_data, 'multipart/form-data'
   response = https.request(request)
